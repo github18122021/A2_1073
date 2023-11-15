3b83afe8c9e0b13ef8f499110105287e388614d9
github18122021 <vrajbagadiya0071@gmail.com>
2023-11-14 22:06:48 -0500

    Finished

--- assignment-2-template/js/gallery.js
+++ assignment-2-template/js/gallery.js
@@ -1,44 +1,44 @@
-let thumbnailImages = document.querySelectorAll('li>img');
-let largeImage = document.querySelector('figure>img');
-let captionInFig = document.querySelector('figure>figcaption');
+// Selecting elements
+const thumbnailImages = document.querySelectorAll('li>img');
+const largeImage = document.querySelector('figure>img');
+const captionInFig = document.querySelector('figure>figcaption');
 
-let galleryItems = [
+// Gallery items data
+const galleryItems = [
     {
         large: 'images/flowers-pink-large.jpg',
-        description: "A field of vibrant sunflowers in the hamlet of Dernekamp, Kirchspiel, Dülmen, Germany.",
+        description: "A field of vibrant sunflowers in Dernekamp, Kirchspiel, Dülmen, Germany."
     },
     {
         large: 'images/flowers-purple-large.jpg',
-        description: "A picturesque cornfield adorned with red poppies in Dülmen, North Rhine-Westphalia, Germany.",
+        description: "A picturesque cornfield with red poppies in Dülmen, North Rhine-Westphalia, Germany."
     },
     {
         large: 'images/flowers-red-large.jpg',
-        description: "A field of vibrant sunflowers in the hamlet of Dernekamp, Kirchspiel, Dülmen, Germany.",
+        description: "A field of vibrant sunflowers in Dernekamp, Kirchspiel, Dülmen, Germany."
     },
     {
         large: 'images/flowers-white-large.jpg',
-        description: "A picturesque cornfield adorned with red poppies in Dülmen, North Rhine-Westphalia, Germany.",
+        description: "A picturesque cornfield with red poppies in Dülmen, North Rhine-Westphalia, Germany."
     },
     {
         large: 'images/flowers-yellow-large.jpg',
-        description: "A picturesque cornfield adorned with red poppies in Dülmen, North Rhine-Westphalia, Germany.",
+        description: "A picturesque cornfield with red poppies in Dülmen, North Rhine-Westphalia, Germany."
     },
     // Add objects for other images
 ];
 
-for (let i = 0; i < thumbnailImages.length; i++) {
-    thumbnailImages[i].addEventListener("click", (e) => {
-        largeImage.src = galleryItems[i].large;
-        captionInFig.textContent = '"' + galleryItems[i].description + '"';
-        let index = i;
+// Click event listeners for thumbnail images
+thumbnailImages.forEach((thumbnail, index) => {
+    thumbnail.addEventListener("click", () => {
+        // Update large image and caption
+        largeImage.src = galleryItems[index].large;
+        captionInFig.textContent = `"${galleryItems[index].description}"`;
 
-        for (let j = 0; j < thumbnailImages.length; j++) {
-            if (j !== index) {
-                thumbnailImages[j].classList.add("grayscale");
-            } else {
-                thumbnailImages[j].classList.remove("grayscale");
-            }
-        }
-        
+        // Apply grayscale effect to non-selected thumbnails
+        thumbnailImages.forEach((thumb, j) => {
+            thumb.classList.toggle("grayscale", j !== index);
+        });
     });
-}
+});
+
